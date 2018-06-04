22f8b8eeff81fe4873e3900c5b3ceb4c109664d6
geovani.mbandeira <geovani.mbandeira@TAT0541209W10-1.senacsp.edu.br>
2018-06-04 16:33:12 -0300

    Aula do dia 04/06/2018

---
+++ Fundamentos do Java/src/br/com/java/CalculadoraLucro.java
@@ -0,0 +1,24 @@
+package br.com.java;
+
+import java.util.Scanner;
+
+public class CalculadoraLucro {
+
+	public static void main(String[] args) {
+		double custo, lucro, venda;
+		Scanner teclado = new Scanner(System.in);
+		System.out.print("Calculadora do lucro");
+		System.out.println("");
+		System.out.print("Preço de custo: ");
+		custo = teclado.nextDouble();
+		System.out.print("% de lucro: ");
+		lucro = teclado.nextDouble();
+		// Processamento
+		venda = custo + ((custo * lucro) / 100);
+		// Saída
+		System.out.println("Preço de venda: " + venda);
+		
+
+	}
+
+}
---
+++ Fundamentos do Java/src/br/com/java/PDV.java
@@ -0,0 +1,31 @@
+package br.com.java;
+
+import java.util.Scanner;
+
+public class PDV {
+
+	public static void main(String[] args) {
+		double custo, desconto, total, troco;
+		Scanner teclado = new Scanner(System.in);
+		System.out.println("Calculadora de desconto");
+		//Entrada
+		System.out.print("Preço de Custo: ");
+		custo = teclado.nextDouble();
+		System.out.print("% de Desconto: ");
+		desconto = teclado.nextDouble();
+		//Processamento
+		total = custo - ((custo * desconto) /100);
+		//Saída
+		System.out.println("Preço a pagar: " + total);
+		//Disconto
+		troco = + (custo - total);
+		System.out.println("Desconto: " + troco);
+	    //Troco
+		System.out.print("Valor Recebido: ");
+		troco = teclado.nextDouble();
+		troco = + (troco - total);
+		System.out.println("Troco ao cliente: " + troco);
+		
+	}
+
+}
